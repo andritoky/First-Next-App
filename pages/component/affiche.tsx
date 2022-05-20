e8d2ee153a0aa795d88a7af58963ab1f08738903
andritoky <randriamampionona9@gmail.com>
2022-05-20 10:58:56 +0200

    first

---
+++ pages/component/affiche.tsx
@@ -0,0 +1,9 @@
+import 'bootstrap/dist/css/bootstrap.css'
+import Bouton from './bouton'
+
+let Affiche: React.FC = () => {
+    return (
+        <div className='alert alert-primary'><Bouton /></div>
+    )
+}
+export default Affiche
---
+++ pages/component/bouton.tsx
@@ -0,0 +1,8 @@
+// import 'bootstrap/dist/css/bootstrap.css'
+
+let Bouton: React.FC = () => {
+    return (
+        <button className='btn btn-dark'> Test</button>
+    )
+}
+export default Bouton
